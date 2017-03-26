da538e2577dc8c6bc775656873d52af98d615364
ttarnok <tarnok.tamas@allwin.hu>
2017-03-26 12:23:39 +0200

    Setup doc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Meetup.Unicorn.Demo
-Unicorn demo project for meetup
+Unicorn demo project for meetup.
+
+## Setup
+ 1. Open the `\src\Meetup.Foundation.Serialization\App_Config\Include\Foundation\Foundation.Serialization.config` and change the `sourceFolder` setting to your absolute path
+ 2.  Open the `\build\unicorn-sync.ps1` and change the `ScriptPath` to your absolute path
+ 3. Install Sitecore to the `\sitecore`  folder
+
--- build/unicorn-sync.ps1
+++ build/unicorn-sync.ps1
@@ -2,7 +2,9 @@ $ErrorActionPreference = 'Stop'
 
 # This is an example PowerShell script that will remotely execute a Unicorn sync using the new CHAP authentication system.
 
-Import-Module c:\Works\Meetup.Unicorn.Demo\build\unicorn.psm1
+$ScriptPath = 'c:\Works\Meetup.Unicorn.Demo\build\unicorn.psm1'
+
+Import-Module $ScriptPath
 
 # SYNC ALL CONFIGURATIONS
 Sync-Unicorn -ControlPanelUrl 'http://unicorn.demo/unicorn.aspx' -SharedSecret 'ED33748F03202A84FC175B88A5CE566942346E3249316548D23D2E3832F46044'
